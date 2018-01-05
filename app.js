3d2f81be0804c2e3b0c0688cfed12a32cba93d10
jonkrause <jonathonkrause@gmail.com>
2018-01-04 19:51:06 -0500

    simple chain validation

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const SHA256 = require('crypto-js/sha256')
 
 class Block {
-  constructor(index, timestamp, data, previoushash = '') {
+  constructor(index, timestamp, data, previousHash = '') {
     this.index = index
     this.timestamp = timestamp
     this.data = data
-    this.previoushash = previoushash
+    this.previousHash = previousHash
     this.hash = this.calculateHash()
   }
 
   calculateHash() {
     return SHA256(
       this.index +
-      this.previoushash +
+      this.previousHash +
       this.timestamp +
       JSON.stringify(this.data)).toString()
   }
@@ -32,14 +32,37 @@ class Blockchain {
   }
 
   addBlock(newBlock) {
-    newBlock.previoushash = this.getLatestBlock().hash
+    newBlock.previousHash = this.getLatestBlock().hash
     newBlock.hash = newBlock.calculateHash()
     this.chain.push(newBlock)
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i]
+      const previousBlock = this.chain[i - 1]
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false
+      }
+    }
+    return true
   }
 }
 
 let coin = new Blockchain()
-coin.addBlock(new Block(1, "01/04/2017", { amount: 4 }))
-coin.addBlock(new Block(2, "01/05/2017", { amount: 12 }))
+coin.addBlock(new Block(1, "01/04/2017", {amount: 4}))
+coin.addBlock(new Block(2, "01/05/2017", {amount: 12}))
 
-console.log(JSON.stringify(coin, null, 4))
+console.log(coin.isChainValid())
+// coin.chain[1].data = { amount: 100 }
+console.log(coin.isChainValid())
+
+if (coin.isChainValid() === false) {
+  console.log("Error: Blockchain structure invalid")
+} else {
+  console.log(JSON.stringify(coin, null, 4))
+}
